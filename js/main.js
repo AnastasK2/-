41cf5ae06a0fedf961d996231d7182ced75f4fb2
AnastasK2 <ZaporozhetsAnastasia12@gmail.com>
2020-09-10 16:57:08 +0300

    доделала базу игры: столновения, очки, перезагрузка

--- js/main.js
+++ js/main.js
@@ -26,6 +26,8 @@ function getQuantityElements( heightElement ){
 
 start.addEventListener('click' , () => {
     start.classList.add('hide');
+    gameArea.innerHTML = "";
+
     for(let i=0; i < getQuantityElements(100); i++){
         const line = document.createElement('div');
         line.classList.add('line');
@@ -43,8 +45,12 @@ start.addEventListener('click' , () => {
         enemy.style.background = 'transparent url("/image/enemy2.png") center / cover no-repeat';
         gameArea.appendChild(enemy);
     }
+    setting.score = 0;
     setting.start = true;
     gameArea.appendChild(car);
+    car.style.left = (gameArea.offsetWidth /2) - (car.offsetWidth/2);
+    car.style.top = auto;
+    car.style.bottom = '10px';
     setting.x = car.offsetLeft;
     setting.y = car.offsetTop;
     requestAnimationFrame(playGame); //анимация  - отрисовка в следующем фрейме
@@ -75,6 +81,23 @@ function moveRoad() {
 function moveEnemy() { 
     let enemy = document.querySelectorAll('.enemy');
     enemy.forEach((item) => {
+        //реализация столкновения
+        //параметры автомобиля и противника - метод, позволяющий вернуть 
+        //размеры и позиции элемента в виде объекта
+
+        let carRect = car.getBoundingClientRect();
+        let enemyRect = item.getBoundingClientRect();
+
+        if((carRect.top <= enemyRect.bottom) && 
+        (carRect.right >= enemyRect.left) && 
+        (carRect.left <= enemyRect.right) &&
+         (carRect.bottom >= enemyRect.top)){
+            setting.start = false;
+            console.warn('ДТП');
+            start.classList.remove('hide');
+            score.style.top = start.offsetHeight;
+        }
+
         item.y += setting.speed / 2;
         item.style.top = item.y + 'px';
         if(item.y >= document.documentElement.clientHeight){
@@ -83,12 +106,13 @@ function moveEnemy() {
         }
     });
 
- 
  }
 
 
 function playGame() {
     if(setting.start){
+        setting.score += setting.speed;
+        score.innerHTML = 'SCORE:<br>' + setting.score;
         moveRoad();
         moveEnemy();
         if(keys.ArrowLeft && setting.x > 0){
